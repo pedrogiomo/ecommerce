09815ffa0c2ca7a8010e6038f6f87192916690fb
pedrogiomo <pedro.augusto@stoxs.com.br>
2021-05-17 18:03:34 -0300

    criamaos a classe page

---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Hcode;
+//agora vou acessar a classe tpl atraves de seu namespace Rain//
+use Rain\Tpl;
+
+class Page {
+
+private $tpl;
+private $options = [];
+private $default = [
+    "data" => []
+];
+
+    public function __construct($opts = array()){
+       
+       $this->options = array_merge($this->default, $opts);
+       
+        $config = array(
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"] . "/views/",
+            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"] . "/views-cache/",
+            "debug"         => false
+        );
+
+        Tpl::configure( $config );
+
+        $this->tpl = new Tpl;
+        $this->setData($this->options["data"]);
+        $this->tpl->draw("header");
+    }
+    
+    private function setData($data = array())
+    {
+        foreach ($data as $key => $value){
+            $this->tpl->assign($key, $valeu);
+        }
+    }
+    
+    public function setTpl($name, $data = array(), $returnHTML = false)
+    {
+        $this->setData($data);
+        return $this->tpl->draw($name, $returnHTML);
+    }
+
+    public function __destruct(){
+
+        $this->tpl->draw("footer");
+
+    }
+
+}
+
+?>
